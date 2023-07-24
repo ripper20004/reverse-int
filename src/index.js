027017734b015a2c8562a6f8f318c135297ebbf5
ripper20004 <138202435+ripper20004@users.noreply.github.com>
2023-07-24 14:18:56 +0400

    feat: realise reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n).trim();
+    if (str[0] === "-") str = str.slice(1);
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        result = str[i] + result;
+    }
+    return +result
 }
